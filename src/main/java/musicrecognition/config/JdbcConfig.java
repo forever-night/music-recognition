020433bce70245d58289f6cdf70c268dec58839c
forever-night <anna.upintheair@gmail.com>
2016-07-26 09:30:58 +0200

    added spring configs for web

--- src/main/java/musicrecognition/config/JdbcConfig.java
+++ src/main/java/musicrecognition/config/JdbcConfig.java
@@ -1,10 +1,9 @@
 package musicrecognition.config;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -16,16 +15,23 @@ import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("musicrecognition")
-@Import({JdbcConfig.class})
 @EnableTransactionManagement
-public class TestConfig {
+public class JdbcConfig {
+    @Autowired
+    DataSource dataSource;
+    
     @Bean
-    public DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/music_recognition");
-        dataSource.setUsername("user");
-        dataSource.setPassword("user");
-
-        return dataSource;
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource);
+    }
+    
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource);
     }
 }
---
+++ src/main/java/musicrecognition/config/WebConfig.java
@@ -0,0 +1,38 @@
+package musicrecognition.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("musicrecognition.controllers")
+public class WebConfig extends WebMvcConfigurerAdapter{
+    @Bean
+    InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(org.springframework.web.servlet.view.JstlView.class);
+        viewResolver.setContentType("text/html;charset=UTF-8");
+        
+        return viewResolver;
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+        registry.addResourceHandler("/WEB-INF/views/**").addResourceLocations("/views/");
+    }
+    
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
