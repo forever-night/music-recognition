b310c523a8710009607a43211b5f4b5722ee5503
forever-night <anna.upintheair@gmail.com>
2016-08-23 14:49:48 +0200

    separated back-end and front-end messages

--- src/main/java/musicrecognition/util/Global.java
+++ src/main/java/musicrecognition/util/Global.java
@@ -2,12 +2,13 @@ package musicrecognition.util;
 
 public class Global {
     public enum Message {
-        SUCCESS ("Success!"),
-        FIELD_EMPTY ("One of the fields is empty"),
         UNSUPPORTED_TYPE ("Unsupported file type"),
         INTERNAL_ERROR ("Internal server error"),
-        UNEXPECTED ("Unexpected error"),
-        ERROR ("Error");
+        ERROR ("Error"),
+        DUPLICATE("Duplicate entity"),
+        NO_CONTENT("No content"),
+        IO_EXCEPTION("Error in reading file"),
+        CANNOT_FINGERPRINT("Cannot fingerprint file");
         
         
         private final String message;
@@ -16,6 +17,26 @@ public class Global {
             this.message = message;
         }
         
+        public String getMessage() {
+            return this.message;
+        }
+    }
+    
+    public enum UIMessage {
+        SUCCESS ("Success!"),
+        FIELD_EMPTY ("One of the fields is empty"),
+        UNSUPPORTED_TYPE ("File is not an audio file or file type is not supported"),
+        DUPLICATE_TRACK("Track already exists"),
+        INTERNAL_ERROR ("Internal server error"),
+        ERROR ("Error");
+    
+    
+        private final String message;
+    
+        UIMessage(String message) {
+            this.message = message;
+        }
+    
         public String getMessage() {
             return this.message;
         }
